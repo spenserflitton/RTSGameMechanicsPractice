b7132156acc1b889ef78fa0abeed2c9ddf82f597
Spenser Flitton <spenser.flitton@gmail.com>
2023-06-02 16:24:03 -0600

    Moved the game directory up one level

---
+++ Source/MyFirstRTS/MyBPLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBPLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTRTS_API UMyBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
