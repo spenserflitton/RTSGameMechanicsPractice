4972c249e593cfb6a88a93c1d0fe171287ab3a0b
Spenser Flitton <spenser.flitton@gmail.com>
2023-06-14 14:26:32 -0600

    Updated resource gathering
    Added interruption logic.
    Added resource gathered indicator.

---
+++ Source/MyFirstRTS/UnitActions.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UnitActions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTRTS_API UUnitActions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
